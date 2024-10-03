d8375964cf9e495768dacde3af72b2c958b1cb16
Roksid2002 <roksid09@gmail.com>
2024-10-03 23:41:35 +0200

    aggiunta server

--- server.py
+++ server.py
@@ -13,5 +13,5 @@ def uploadfiles():
 
     return {"message": "File ricevuti correttamente."}, 200
 
-if __name__ == '__main':
+if __name__ == '__main__':  # Corretto: aggiungi il secondo trattino basso
     app.run(host='0.0.0.0', port=5000)
